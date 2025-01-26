8e5ac745fc8c222c2537fb0e50286a9056b4994f
Austin Keiber <adkeiber@gmail.com>
2025-01-26 04:24:51 -0500

    step 2.1

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TIME_ZONE = 'America/New_York'
